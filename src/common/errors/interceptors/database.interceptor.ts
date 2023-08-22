f98608757ab31d314881bb5f9dead78523f12ce3
Trevezan <b39s@hotmail.com>
2023-08-22 10:28:16 -0300

    feat: "database interceptor"

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -1,11 +1,20 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, BadRequestException } from '@nestjs/common';
 import { Observable, catchError } from 'rxjs';
+import { isPrismaError } from '../utils/is-prisma-error.util';
+import { handleDatabaseErros } from '../utils/handle-database-erros.utils';
+import { DatabaseError } from '../types/DatabaseError';
 
 @Injectable()
 export class DatabaseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(error => {
+        if (isPrismaError(error)) {
+          error = handleDatabaseErros(error);
+        }
+        if (error instanceof DatabaseError) {
+          throw new BadRequestException(error.message);
+        }
         throw error;
       }),
     );
