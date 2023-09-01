3eb811f0e1149e7287e4bf63d7e1bd7a9720a508
Trevezan <b39s@hotmail.com>
2023-09-01 10:06:46 -0300

    feat: "Tags in swagger"

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -2,8 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
-import { ApiForbiddenResponse } from '@nestjs/swagger';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Posts')
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
